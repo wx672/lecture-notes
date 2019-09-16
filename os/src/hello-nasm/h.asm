ffee65d329661eb6890e47340fd2c74251ff95a9
Wang Xiaolin <wx672ster@gmail.com>
2019-09-16 17:07:47 +0800

    *** empty log message ***

--- os/src/hello-nasm/h.asm
+++
@@ -1,7 +0,0 @@
-Msg: db 'Hello, world'
-MsgLen: equ $-Msg
-mov eax, 4       ; 4 = sys_write 
-mov ebx, 1       ; 1 = STDOUT
-mov ecx, Msg     ; address of message
-mov edx, MsgLen  ; length of string
-int 80h          ; call the kernel
